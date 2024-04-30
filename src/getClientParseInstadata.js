d5c7604960a76dc9ee6be13ca08a5c569bd26ad3
pavlos noulis <pavlos.noulis@instashop.ae>
2024-04-30 11:36:55 +0300

    feat: input validation

--- src/getClientParseInstadata.js
+++ src/getClientParseInstadata.js
@@ -17,6 +17,15 @@ function getClientParseInstadata(options) {
   options.appId = options.appId || process.env.PARSE_APP_ID;
   options.jsKey = options.jsKey || process.env.PARSE_JS_KEY;
   options.masterKey = options.masterKey || process.env.PARSE_MASTER_KEY;
+
+  if (
+    !(options.serverUrl && options.appId && options.jsKey && options.masterkey)
+  ) {
+    throw new TypeError(
+      `Invalid argument 'options:${JSON.stringify(options)}'`,
+    );
+  }
+
   Parse.initialize(options.appId, options.jsKey, options.masterKey);
   Parse.serverURL = options.serverUrl;
   return Parse;
